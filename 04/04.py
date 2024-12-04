22fd532facce9a875101f601a0e507623a439298
AdamBeedell <114735333+AdamBeedell@users.noreply.github.com>
2024-12-04 22:56:35 +0000

    first part solved, got through the logic OK but used aid to bugfix while running short on time. This .py shouldnt be run, it's got some unhashed code doing competeing jobs as i wasnt happy with any solution

--- 04/04.py
+++ 04/04.py
@@ -2,9 +2,7 @@
 
 ### word search for xmas
 
-
-input = """
-XXASMSMSXMXSMMXMAXMMMAMSAMASMSAMXMAXSXMASMAMMXXMASASXAMAMXSXMMSXSAMXSSXXMXAAXMAXMXXSXMXMMAAMMMMMMMMMMXXMSMSSSMXAAMAMMMSAMXXXXSMSAXMMXXMASMSS
+input = """XXASMSMSXMXSMMXMAXMMMAMSAMASMSAMXMAXSXMASMAMMXXMASASXAMAMXSXMMSXSAMXSSXXMXAAXMAXMXXSXMXMMAAMMMMMMMMMMXXMSMSSSMXAAMAMMMSAMXXXXSMSAXMMXXMASMSS
 ASMMAAAMAMASAXSSMMSASAMXAAAAXXXMAXXAMSASXMASMSSMSAMSAMMSXMAXSMAAMMSAMAAMSMSMSSSSXSXSMSAAXMXMAAAAMMSAMXSXMAAAAAXMAMAMAMAMMXMSASAXMXSAMXSAMXAS
 XSAMSMMSAMASXMXAAXSASXXSSMXXXMMSMSMSMAMMASASXAAXXAMXAXAMAXMASMMSMAMMSMSMAAAAAAAAASASASXSMSASXSSXSAXAMAMAMMMSMMMMAXASMSMXSAAMAMXMMAMAXXAMXMMS
 AMAMXAASMMXSAMSMMMMMMMMMAMMSMMAAAAAAXAXSAMASMSSMSAMSAMMSMMMXXAAAMXMAAAAMXMMMMSMMMMAMAMAMAMMSXMAXMASXMSSMSAMXAAASMSMXAAXAMMSMAAMAMXSMMSASXSXM
@@ -143,7 +141,220 @@ MAMMXMAMMSMMMSMMASXMAXAAXXMASASMAMMMMSMMMMMXMXAXMSMSMSMSXXXAAXSXXSASAAXMMMAMMAMA
 SAMSAMXMMASAXASAXMAMXSXSAMXSMAXMAMASAAAXSAMASXSAMXAXAAAAMSAMSXXAXSASXSMSAMXXXMMMSMXAAMMAMXMAXSAXAAXMXMMSXMASAMSAAMAMXXSSXMSAMMMSSMAMASXMSMMS
 SAXSASMASAMMSAXMMSAMMMAXMAMXMXMSXSASXSMMMAMXSAXAMMMMMMMAMSAAXAMXMMAMAXAMSSXMMSMMMMSSSSSXMXMSXMASMSSMMSMMMSXSASXMSSXMAAXMASAAXSAMXMXSXMXAAMAM
 SMMSAMXAMSSXMMMSAMXSAAAMMSAXMAXAMMAMAAAXSAMXMAMASAAAXAMSAMXMMAMMSXAMMMAMASAMAAAASAMAAAAAXMAMAAASAMXXAAAAAMMMXMXMAXASXSMSXMSSMMXMSAMXXSMSMSAA
-XSAMMMMMSXMXAMXMXSMXXMSSXASAMXMSSMMMSMAMAMXXMSMSSMSSSXSAMXMXSAMXASASXSXMXSMMSSSMSXMMMMSAMXSSMMASMMSMSSSMXSAMXAXMASAMSAMXMAXMASXAAXMSAMXMASMS
-"""
+XSAMMMMMSXMXAMXMXSMXXMSSXASAMXMSSMMMSMAMAMXXMSMSSMSSSXSAMXMXSAMXASASXSXMXSMMSSSMSXMMMMSAMXSSMMASMMSMSSSMXSAMXAXMASAMSAMXMAXMASXAAXMSAMXMASMS"""
 
-##### make a variable that's a grid of these
+search="XMAS"
+
+##### Make a variable that's a grid of these
+
+inputsplitlines = input.splitlines()
+grid = []
+
+for line in inputsplitlines:
+    grid.append(line)
+
+## its a 140x140 grid
+
+##### Intend to iterate over each letter, looking in all 8 directions for instances of XMAS
+
+##### I would need to dedupe items like XMAS from counting from both ends... or actually I can just check all the Xs instead of each letter
+
+##### Check if the nearby objects are in bounds
+
+def index_exists(lst, index):
+    return 0 <= index and index < len(lst)
+
+##### check if the next char in the string exists
+
+#def wordsearch(target, currentchar, chartovalidate): #### this fails if we're searching for xxx in xxxxxxx, would need another index in there i guess, ie, assuming the values in target are unique
+#    if currentchar == target[-1]: ##if at S in xmas
+#        return False, True
+#    else: 
+#        return chartovalidate == target[target.index(currentchar)+1], False
+
+
+##### Eight way check given a coord
+
+#def eightway(y,x):
+#    return [[y, x + 1], [y, x - 1], [y - 1, x], [y + 1, x],
+#            [y - 1, x - 1], [y - 1, x + 1], [y + 1, x - 1], [y + 1, x + 1]]
+
+### eightway(5,5) 
+### [[5, 6], [5, 4], [4, 5], [6, 5], [4, 4], [4, 6], [6, 4], [6, 6]]
+
+##### Iterate over all Xs
+
+
+#y=0
+#for line in grid:
+#    x=0
+#    for char in line:
+#        if char == "X":
+#            for direction in eightway(y,x):
+#                #print (direction[0],direction[1])
+#                ty = y+direction[0]
+#                tx = x+direction[1]
+#                if index_exists(grid,ty) & index_exists(grid[ty],tx):
+#                    checknext = wordsearch("XMAS", char, grid[ty][tx])
+#                    if checknext[1] == True:
+#                        
+#            #wordsearch("XMAS",char,)
+#        x+=1
+#    y+=1
+
+
+
+
+
+
+#for i in range(0, len(grid[0])):
+
+##### if I'm at [5],[5] I need to check
+# [5,6] right       [y][x+1]  
+# [5,4] left        [y][x-1]
+# [4,5] up          [y-1][x]
+# [6,5] down        [y+1][x]
+# [4,4] up left     [y-1][x-1]
+# [4,6] up right    [y-1][x+1]
+# [6,4] down left   [y+1][x-1]
+# [6,6] down right  [y+1][x+1]
+
+#### finding the value is a coord is just grid[y][x]
+
+##### scratch that better plan
+
+#[5.5]
+
+def eightwayfull(y,x,find): ### pull the next n(4) chars in all directions if they exist
+    east=[]
+    west=[]
+    north=[]
+    south=[]
+    nw=[]
+    ne=[]
+    sw=[]
+    se=[]
+    eightwaysearched = []
+    for i in range(0, len(find)):
+        if index_exists(grid, y) & index_exists(grid[y], x+i): east.append(grid[y][x+i])
+        if index_exists(grid, y) & index_exists(grid[y], x-i): west.append(grid[y][x-i])
+        if index_exists(grid, y-i) & index_exists(grid[y], x): north.append(grid[y-i][x])
+        if index_exists(grid, y+i) & index_exists(grid[y], x): south.append(grid[y+i][x])
+        if index_exists(grid, y-i) & index_exists(grid[y-i], x-i): nw.append(grid[y-i][x-1])
+        if index_exists(grid, y-i) & index_exists(grid[y-i], x+i): ne.append(grid[y-i][x+i])
+        if index_exists(grid, y+i) & index_exists(grid[y+i], x-i): sw.append(grid[y+i][x-i])
+        if index_exists(grid, y+i) & index_exists(grid[y+i], x+i): se.append(grid[y+i][x+i])
+        eightwaysearched.append(east)
+        eightwaysearched.append(west)
+        eightwaysearched.append(north)
+        eightwaysearched.append(south)
+        eightwaysearched.append(nw)
+        eightwaysearched.append(ne)
+        eightwaysearched.append(sw)
+        eightwaysearched.append(se)
+    return eightwaysearched
+
+
+## iterate through Xs in grid
+
+searchresults=[]
+y=0
+for line in grid:
+    x=0
+    for char in line:
+        if char == "X":
+            searchresults.append(eightwayfull(y,x,search))
+        x+=1
+    y+=1
+
+
+## this doesnt work, but the core of the logic was about there, refactored to the below with some LLM aid
+
+
+### count matches in the sequences returned
+#matches = 0
+#for result in searchresults:
+#    if re.match(result, rf"{search}"): matches += 1
+#print(matches)
+
+# Count matches
+matches = sum(1 for result in searchresults if result == search)
+print(matches)
+
+
+
+
+search = "XMAS"
+
+# Prepare Grid
+inputsplitlines = input.splitlines()
+grid = [list(line) for line in inputsplitlines]  # 2D list for easier indexing
+
+# Check if index is within bounds
+def index_exists(lst, y, x):
+    return 0 <= y < len(lst) and 0 <= x < len(lst[0])
+
+# Collect directions
+def eightwayfull(y, x, find):
+    directions = [
+        (0, 1), (0, -1), (-1, 0), (1, 0),  # East, West, North, South
+        (-1, -1), (-1, 1), (1, -1), (1, 1)  # NW, NE, SW, SE
+    ]
+    results = []
+    for dy, dx in directions:
+        current = []
+        for i in range(len(find)):
+            ny, nx = y + dy * i, x + dx * i
+            if index_exists(grid, ny, nx):
+                current.append(grid[ny][nx])
+            else:
+                break
+        results.append("".join(current))
+    return results
+
+# Iterate through X's in grid
+searchresults = []
+for y in range(len(grid)):
+    for x in range(len(grid[y])):
+        if grid[y][x] == "X":
+            searchresults.extend(eightwayfull(y, x, search))
+
+# Count matches
+matches = sum(1 for result in searchresults if result == search)
+print(matches)
+
+
+
+
+def eightwayfull(y, x, find):
+    # Initialize lists for each direction
+    east = []
+    west = []
+    north = []
+    south = []
+    nw = []
+    ne = []
+    sw = []
+    se = []
+
+    # Iterate to collect characters in all 8 directions
+    for i in range(len(find)):
+        if index_exists(grid, y) and index_exists(grid[y], x + i):
+            east.append(grid[y][x + i])  # Right
+        if index_exists(grid, y) and index_exists(grid[y], x - i):
+            west.append(grid[y][x - i])  # Left
+        if index_exists(grid, y - i) and index_exists(grid[y - i], x):
+            north.append(grid[y - i][x])  # Up
+        if index_exists(grid, y + i) and index_exists(grid[y + i], x):
+            south.append(grid[y + i][x])  # Down
+        if index_exists(grid, y - i) and index_exists(grid[y - i], x - i):
+            nw.append(grid[y - i][x - i])  # Up-left
+        if index_exists(grid, y - i) and index_exists(grid[y - i], x + i):
+            ne.append(grid[y - i][x + i])  # Up-right
+        if index_exists(grid, y + i) and index_exists(grid[y + i], x - i):
+            sw.append(grid[y + i][x - i])  # Down-left
+        if index_exists(grid, y + i) and index_exists(grid[y + i], x + i):
+            se.append(grid[y + i][x + i])  # Down-right
+
+    # Combine all results into a single list
+    return [east, west, north, south, nw, ne, sw, se]
