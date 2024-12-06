8411c77122a2dbc75c2b2f22feb40f5d33748a31
AdamBeedell <114735333+AdamBeedell@users.noreply.github.com>
2024-12-06 20:32:54 +0000

    program runs and guard escapes the map, positions are counted, answer is not correct though

--- 06/06.py
+++ 06/06.py
@@ -130,3 +130,76 @@ map = """.#......................................#................#....#.......#
 .......................#..#.......................#..............#............#..........#........#..............................#
 .....#...............#...##...........#..................#........................................#.....#.....#...................
 ....#.....................................#..........#..........#..#.....#...#....#...#...............#..........................#"""
+
+### there is a map, a guard and obstacles, the guard is marked ^ denoting location + direction
+### guard's rules are as follows:
+## walk till obstacle, then turn 90 degrees right
+## repeat
+
+### we want to iterate through states until the guard leaves the map, and mark his path with Xs
+### count Xs (distinct locations covered)
+
+### gives me a co-ordinates system, probably better ways but it'll do, map[y][x]
+map = map.splitlines()
+
+## Find the guard
+
+def findguard(map):
+    for y, line in enumerate(map):
+        for x, spot in enumerate(line):
+            if spot in ('^','v','>','<'):
+                coords = [y,x] ### I want to pass in/out what the spot's location is within the original map
+                return True, coords
+    return False, None            
+
+
+start = findguard(map)
+
+### gonna use states?
+
+
+## define movement rules
+
+## if no obstruction, update location with coord deltas
+nextdirections = {
+    "^": (-1, 0),
+    "v": (1, 0),
+    ">": (0, 1),
+    "<": (0, -1)
+}
+
+## if obstruction this then above
+nextturn = {
+    "^": ">",
+    ">": "v",
+    "v": "<",
+    "<": "^"
+}
+
+### out of the map check
+def is_within_bounds(y, x, map):
+    return 0 <= y < len(map) and 0 <= x < len(map[0])
+
+### general next move logic. Doing the rotation as a whole move I guess, seems ok to do so.
+def nextmove(y,x, dir, map):
+    ahead = nextdirections[dir]
+    if not is_within_bounds(y+ahead[0],x+ahead[1],map): ## transform applied, off map
+        return (y,x, None, False)
+    if map[y+ahead[0]][x+ahead[1]] == "#": ## transform applied, hit obstacle
+        return(y,x, nextturn[dir], True)
+    else:
+        return(y+ahead[0], x+ahead[1], dir, True)
+
+locations = []
+state = nextmove(start[1][0],start[1][1], map[start[1][0]][start[1][1]], map)
+while state[3] == True:
+    state = nextmove(state[0], state[1], state[2], map)  ## y, x, keeprunning
+    print(f"{state[0]}, {state[1]}, {state[2]}")
+    locations.append([state[0], state[1]])
+#### Going to try and run the program first, then figure out how to track the movement if it does movement properly.
+#### I think i'll add to the map to make a new map maybe?
+#### could also could export the coords of YX to a list, dedupe the list, count those.
+
+unique_locations = list(set(tuple(coord) for coord in locations))
+
+print(len(unique_locations)) 
