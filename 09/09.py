3d9bbb35d0da51db61a112c6698669d2d0f70997
AdamBeedell <114735333+AdamBeedell@users.noreply.github.com>
2024-12-09 11:48:26 +0000

    forgot to hit save, actually + notes.

--- 09/09.py
+++ 09/09.py
@@ -1,3 +1,15 @@
 ### 09.py
 
 diskmap = """2333133121414131402"""
+
+## going to be using that step-by-two function from yesterday
+
+### doing a defrag kinda
+
+### the string of numbers above alternates between digits saying the used blocks and the free blocks, 2 used, 3 free, 3 used, 3 free etc.
+
+### we're to condense out the free space on the left by moving items from the right one at a time
+
+### two pointer block loop?
+
+### then checksum - result = 0, for each int(char) in string, result += i*char
