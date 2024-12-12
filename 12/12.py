4170851576d30d488fd7beca5ee118fd91b62f4b
AdamBeedell <114735333+AdamBeedell@users.noreply.github.com>
2024-12-12 16:13:42 +0000

    added a couple notes for day 12

---
+++ 12/12.py
@@ -0,0 +1,17 @@
+#12.py
+
+input="""AAAA
+BBCD
+BBCC
+EEEC"""
+
+### we're figuring out area and perimeter of areas on the map as defined by boundaries between contiguous letters 
+
+
+## bound A = area (4) * perimeter (4+4+1+1)
+## bound B = area (4) * perimeter (2+2+2+2)
+## bound C = area (3) * perimeter (2+1+1+1+1+2+1+1+1)
+## bound D = area (1) * perimeter (1+1+1+1)
+## bound E = area (3) * perimeter (3+3+1+1)
+
+
